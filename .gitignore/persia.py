08dce3662b9f70621daaa8dd306be566ed47de7b
amir computing software <amirandshamar@gmail.com>
2017-09-24 09:25:15 -0700

    Update and rename .gitignore to persia.py

---
+++ .gitignore/persia.py
@@ -0,0 +1,6 @@
+name = raw_input("what is your name?") 
+print "hello",name
+if name == "amir":
+	print "So glad your helping persia learn interactive stories."
+print "Princess Persia was chilling in her game room choosing what video game she should be playing"
+game = 
